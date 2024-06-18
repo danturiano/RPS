7c8afabbfd8ef2d063193f3511350ab64a2d1a0d
Daniel <165353614+danturiano@users.noreply.github.com>
2024-06-18 14:55:32 +0800

    Add function logic for playing a single round

--- script.js
+++ script.js
@@ -7,16 +7,37 @@ function getComputerChoice() {
     return "Rock";
   } else if (computerChoice == 1) {
     return "Paper";
-  } else return "Scissors";
+  } else return "Scissor";
 }
 
 function getHumanChoice() {
-  let value = prompt("Choose between: Rock, Paper, Scissors");
-  value = value.charAt(0).toUpperCase() + value.slice(1);
-  if (value === "Rock" || value === "Paper" || value === "Scissors") {
-    //WILL ONLY RETURN TRUE IF ONE OF THE CHOICES IS TRUE
-    return value;
-  } else {
-    console.log("Choice is not a valid must be Rock, Paper, or Scissors Only.");
+  let value = prompt("Choose between: Rock, Paper, Scissor");
+  return value;
+}
+
+function playRound(humanChoice, computerChoice) {
+  //Rock beats scissors, scissors beat paper, paper beats rock
+  humanChoice.toLowerCase();
+  switch (humanChoice) {
+    case "rock":
+      if (computerChoice == "Paper") {
+        console.log("You Lose! Paper beats Rock!");
+      } else console.log("You Win");
+      break;
+    case "paper":
+      if (computerChoice == "Scissor") {
+        console.log("You Lose! Scissor beats Paper!");
+      } else console.log("You Win");
+      break;
+    case "scissor":
+      if (computerChoice == "Rock") {
+        console.log("You Lose! Rock beats Scissor!");
+      } else console.log("You Win");
+      break;
   }
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
