ebab7421e18e8fc3cd2ffd71e23b7368bae95469
Daniel <165353614+danturiano@users.noreply.github.com>
2024-06-18 13:53:41 +0800

    declare variables for human and computer score

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let humanScore,
+  computerScore = 0;
+
 function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * 3); //RETURNS 0,1,2
+  const computerChoice = Math.floor(Math.random() * 3); //RETURNS 0,1,2
   if (computerChoice == 0) {
     return "Rock";
   } else if (computerChoice == 1) {
