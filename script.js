115a0bba1ffa354f2433900c9c79f6e55a000756
Daniel <165353614+danturiano@users.noreply.github.com>
2024-06-18 13:46:27 +0800

    Add working logic to getHumanChoice function

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function getComputerChoice() {
     return "Paper";
   } else return "Scissors";
 }
+
+function getHumanChoice() {
+  let value = prompt("Choose between: Rock, Paper, Scissors");
+  value = value.charAt(0).toUpperCase() + value.slice(1);
+  if (value === "Rock" || value === "Paper" || value === "Scissors") {
+    //WILL ONLY RETURN TRUE IF ONE OF THE CHOICES IS TRUE
+    return value;
+  } else {
+    console.log("Choice is not a valid must be Rock, Paper, or Scissors Only.");
+  }
+}
