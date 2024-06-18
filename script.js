7c69245c7fec0690f0724dd8cb4c540bb0540e60
Daniel <165353614+danturiano@users.noreply.github.com>
2024-06-18 15:34:16 +0800

    Update playRound function possibility for a draw

--- script.js
+++ script.js
@@ -15,24 +15,33 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
   //Rock beats scissors, scissors beat paper, paper beats rock
   humanChoice.toLowerCase();
-  let humanWinner = true;
+  let humanWinner = 1;
   switch (humanChoice) {
     case "rock":
       if (computerChoice == "Paper") {
         console.log("You Lose! Paper beats Rock!");
-        humanWinner = false;
+        humanWinner = -1;
+      } else if (computerChoice == "Rock") {
+        console.log("Draw, You both Picked Rock.");
+        humanWinner = 0;
       } else console.log("You Win");
       return humanWinner;
     case "paper":
       if (computerChoice == "Scissor") {
         console.log("You Lose! Scissor beats Paper!");
-        humanWinner = false;
+        humanWinner = -1;
+      } else if (computerChoice == "Paper") {
+        console.log("Draw, You both Picked Paper.");
+        humanWinner = 0;
       } else console.log("You Win");
       return humanWinner;
     case "scissor":
       if (computerChoice == "Rock") {
         console.log("You Lose! Rock beats Scissor!");
-        humanWinner = false;
+        humanWinner = -1;
+      } else if (computerChoice == "Scissor") {
+        console.log("Draw, You both Picked Scissor.");
+        humanWinner = 0;
       } else console.log("You Win");
       return humanWinner;
   }
@@ -41,18 +50,23 @@ function playRound(humanChoice, computerChoice) {
 function playGame() {
   let humanScore = 0;
   let computerScore = 0;
+  let drawCount = 0;
   let rounds = 0;
   while (rounds != 5) {
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
     let winner = playRound(humanSelection, computerSelection);
-    if (winner) {
+    if (winner == 1) {
       humanScore++;
+    } else if (winner == 0) {
+      drawCount++;
     } else computerScore++;
     rounds++;
   }
   if (humanScore > computerScore) {
     console.log("You are the Winner!");
+  } else if (drawCount == 5) {
+    console.log("There is no Winner!");
   } else console.log("You Lose, Try Again Next Time!");
 }
 
